dbbd07d5e895904eac9fef5889b45f37d1366e9f
fiapluiztadeu <111822541+fiapluiztadeu@users.noreply.github.com>
2025-05-16 19:22:16 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true
-  },
-  typescript: {
-    ignoreBuildErrors: true, // ⚠️ ignora erros de TS — use com cautela
-  },
+  
 };
 
 export default nextConfig;
